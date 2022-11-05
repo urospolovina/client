eecb1da81dd2a3c4416a61a980a6ff8cd79ba9ff
urospolovina <uros.polovina@gmail.com>
2022-11-05 13:56:04 +0100

    client

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <a href="#" @click.prevent="login">Login</a>
+        <span>{{ $auth.user }}</span>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+    data (){
+        return {
+           
+        }
+    },
+
+    methods: {
+        login () {
+            this.$auth.loginWith('laravelSanctum', {
+                data: {
+                    email: 'uros@gmail.com',
+                    password: 'password'
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
