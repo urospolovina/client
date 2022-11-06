45156493b778f051587fbe1b84434cf8bf092056
urospolovina <uros.polovina@gmail.com>
2022-11-06 12:03:37 +0100

    crtice bez

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,12 +37,12 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/auth-next',
   ],
-
+  // Uros
   auth: {
     strategies: {
       'laravelSanctum': {
         provider: 'laravel/sanctum',
-        url: 'https://mlohapi.herokuapp.com/',
+        url: 'https://mlohapi.herokuapp.com',
         endpoints: {
           login: {
             url: '/api/login',
