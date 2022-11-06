f4af0c618fb53f1634f12a794e49a1a306a27fe3
urospolovina <uros.polovina@gmail.com>
2022-11-06 12:15:53 +0100

    sad

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <a href="#" @click.prevent="login">Login</a> <br>
-        <span>{{ $auth.user.name }}</span>
+        <span>{{ $auth.user }}</span>
     </div>
   
 </template>
